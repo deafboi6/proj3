7069a98c9d429ccf42642fa838987079f30a335d
Mathew Chandler <Cascabelrock@gmail.com>
2023-03-14 19:09:56 -0700

    added time to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
+import React, { useEffect, useRef, useState } from "react";
 
+export default function App() {
+  const newDate = useRef(new Date());
 
-function App() {
+  const [currentHours, setCurrentHours] = useState(newDate.current.getHours());
+  const [currentMinutes, setCurrentMinutes] = useState(
+    newDate.current.getMinutes()
+  );
+  const [currentSeconds, setCurrentSeconds] = useState(
+    newDate.current.getSeconds()
+  );
+  setInterval(() => {
+    newDate.current = new Date();
+    setCurrentHours(newDate.current.getHours());
+    setCurrentMinutes(newDate.current.getMinutes());
+    setCurrentSeconds(newDate.current.getSeconds());
+  }, 1000);
+  const nowTime = `${currentHours} : ${currentMinutes} : ${currentSeconds}`;
+  useEffect(() => {
+    console.log(nowTime);
+  });
   return (
     <div>
-      <h1>Start of our project 3!</h1>
+      <h2>This is the Start of Project 3!</h2>
+      <h3>{nowTime}</h3>
     </div>
   );
 }
-
-export default App;
