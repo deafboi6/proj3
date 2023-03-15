35aa8fa36d633f0601e7befccb287ecac29a4df4
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-14 19:57:58 -0700

    prepped changes for pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,47 @@
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+} from "chart.js";
+
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend
+);
+
 
 
 function App() {
+  // ----ON STANDBY UNTIL HOMEPAGE IS READY---- //
+  // const home = {
+  //   name: "Home",
+  //   amount: 15
+  // };
+  
+  // const data = {
+  //   labels: [home.name, "Groceries", "Insurance", "Car payment", "Utilities", "Savings", "Other"],
+  //   datasets: [
+  //     {
+  //       data: [home.amount, 17, 12, 21, 7, 19, 5],
+  //       backgroundColor: ["blue", "orange", "purple", "red", "green", "chocolate", "aqua"],
+  //       cutout: "50%"
+  //     },
+  //   ]
+  // };
+
+  // const options = {};
+
   return (
     <div>
       <h1>Start of our project 3!</h1>
+      <div>
+        {/* ON STANBY UNTIL HOME PAGE IS READY */}
+        {/* <Pie data = {data} options= {options} /> */}
+      </div>
     </div>
   );
 }
