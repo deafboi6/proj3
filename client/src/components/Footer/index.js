1fd1d450274b68e19a44a273169048d4105bddaf
“Spencer <srye1140@gmail.com>
2023-03-16 19:11:32 -0700

    adds month dropdown

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,12 +5,14 @@ const Footer = () => {
   return (
     <footer className="">
       <ul>
-        <Link to="https://github.com/ArturoSer">Arturo's GitHub</Link>
-        <Link to="https://github.com/walleric23">Eric's GitHub</Link>
-        <Link to="https://github.com/deafboi6">Jayden's GitHub</Link>
-        <Link to="https://github.com/Stingxming">Mathew's GitHub</Link>
-        <Link to="https://github.com/Syre11">Spencer's GitHub</Link>
+        <li><Link to="https://github.com/ArturoSer">Arturo's GitHub</Link></li>
+        <li><Link to="https://github.com/walleric23">Eric's GitHub</Link></li>
+        <li><Link to="https://github.com/deafboi6">Jayden's GitHub</Link></li>
+        <li><Link to="https://github.com/Stingxming">Mathew's GitHub</Link></li>
+        <li><Link to="https://github.com/Syre11">Spencer's GitHub</Link></li>
       </ul>
+      <p>*not actually guaranteed to make you a millionaire.</p>
+
     </footer>
   )
 }
