98cb2d15e16b6ee7164a7cd72c52faf6ea250300
walleric23 <walleric23@gmail.com>
2023-03-16 19:28:00 -0700

    user model, resolvers and typeDefs

---
+++ server/models/User.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
